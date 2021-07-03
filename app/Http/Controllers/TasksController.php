1448cbfb0296869550e95a33dc2d0e44807ee150
taro.yamada <do.learning00@gmail.com>
2021-07-03 05:29:50 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -25,8 +25,14 @@ class TasksController extends Controller
 
     public function store(Request $request)
     {
+        //バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+        ]);
+        
         //タスクを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -55,9 +61,15 @@ class TasksController extends Controller
 
     public function update(Request $request, $id)
     {
+        //バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+        ]);
+        
         //idを検索して取得
         $task = Task::findOrFail($id);
         
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
